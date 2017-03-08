a8acbf782cc3f1df2976be56f64c058fa87c2399
xjh <767577465@qq.com>
2017-03-08 18:40:52 +0800

    重命名
    重命名

---
+++ myoffer/MyOffer/Class/home/XiaoBai/controller/GongLueTip.h
@@ -0,0 +1,16 @@
+//
+//  GongLueTip.h
+//  myOffer
+//
+//  Created by xuewuguojie on 2017/3/8.
+//  Copyright © 2017年 UVIC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GongLueTip : NSObject
+@property(nonatomic,copy)NSString *avatar;
+@property(nonatomic,copy)NSString *title;
+@property(nonatomic,copy)NSString *content;
+
+@end
---
+++ myoffer/MyOffer/Class/home/XiaoBai/controller/GongLueTip.m
@@ -0,0 +1,20 @@
+//
+//  GongLueTip.m
+//  myOffer
+//
+//  Created by xuewuguojie on 2017/3/8.
+//  Copyright © 2017年 UVIC. All rights reserved.
+//
+
+#import "GongLueTip.h"
+
+@implementation GongLueTip
+
+- (void)setTitle:(NSString *)title{
+
+    _title = title;
+    
+}
+          
+
+@end
